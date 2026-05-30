a4ac2d071ba362c125bd368fd8d30ee6fc8d35bb
bismarkgyimah92-bit <bismarkgyimah92@gmail.com>
2026-05-30 17:53:55 +0000

    Assignment 1

--- initials.py
+++ initials.py
@@ -1,7 +1,37 @@
+# File:  Initial.py
+# Description:  Print out my initials
+# Assignment Number: 1
+
+# Name:  Gyimah Bismark
+# Student ID:2425402264
+# Email: 2425402264@live.gctu.edu.gh
+#Grader:Mr Augustus 
+
+# On my honor,Gyimah Bismark,this programming ,
+# assignment is my own work
+# and I have not provided this code to any other 
+# student.
 
 def main():
-  pass #code goes here
+    # Writing a script to print the block letter of my name
 
-
-
+    print('''       
+    ...GB    
+        
+    .....GGGGGGGGGG........BBBBBBBBBB.....
+    ....GGGGGGGGGGG........BB.......BB....                         
+    ...GGG.................BB.......BBB...                                              
+    ...GG..................BB.......BB....                                   
+    ...GG..................BBBBBBBBBB.....                            
+    ...GG... GGGGGG........BBBBBBBBB......                     
+    ...GG........GG........BB.......BB....                             
+    ...GGG.......GG........BB.......BBB...                           
+    ....GGGGGGGGGGG...**...BB ......BB....                     
+    .....GGGGGGGGGG...**...BBBBBBBBBB.....                  
+                  
+  ''')
+    
 main()
+
+
+
